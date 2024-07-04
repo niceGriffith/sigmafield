0c783bba9503e2cef3166e12dda777c19370d0ae
Debaditya Chakraborty <chakrobortydebaditya@gmail.com>
2024-07-04 17:25:45 +0530

    Added some Animations

---
+++ anim2.js
@@ -0,0 +1,31 @@
+var delay=100
+var fcontent=new Array()
+begintag='<font face="Lucida Console" size=3>'
+ 
+fcontent[0]="                                                  <br>                                                  <br>            .7Y,              .BBBBY              <br>            MB18:  k17        UBkUNB              <br>           iBqi7  jMi7      :L8Bqrur              <br>         .GBBBBBLXBBBB:    BBBBBMBBBEr            <br>         BMBMMBBqZOZOBBq. BMMBBOBOBBBL;, :        <br>         B0NP8P8O8E0U0BB:jBqN8q0NGS7BBBBBB2Li,    <br>        .BM0E01UBBMFSF:55BBMM0GZEBGSBMMBk .iUqM   <br>         BjkFLJMBBSOBr  ,BqMFkq5kB:.BBBB,         <br>        ZBONZGM Y8GFS   :BS77M2UMu  .BBB          <br>        qBB8kZB   BXi   qBZESqXZ.                 <br>         .JBG2B   BBX ,ZBGG8BMBB2                 <br>           Nqi8  BBOF UBG1PBBBMkqBr               <br>          kBB8MkEB57BiGFS2BN YBBEBBi              <br>          kBBOYBBBJ.MB7P1EB,   8BBXB.             <br>          ;BBBi JS7 :B8OZMO     MBBBY             <br>BMMMZq527.UBZ:    .BGBBBBO      7BEBY             <br>jU0ZMBBM0XBPM.    .BBu7::       ,BZMB             <br>     ,rjY1BB1.    .BM            BB8B.            <br>          :i,    7JU5:ii;i:.      MBB             <br>             .   UXY7uU22kPNS1vi  ,BBr            <br>                   :iirii,. :iLYu: LBBBB.         <br>" 
+fcontent[1]="                                                  <br>                                                  <br>            :FX7               715U.              <br>            MB7Pr  rY,        iB88BB              <br>           7BOrL  ;M;L        qMPiuU              <br>         :BBBBBBPuBBkS     r8BBBZNEu:r            <br>         B8OOEBBPZMMBBBr  7BMBBMBBBBB:            <br>         BMNPOXkM8Z8UPBBi:BZqB0ENGq0BBMMXB.       <br>         BGqqk5uMBGPEMqB:1BZ8ZEEZMZ7MBBBB7UXP1L   <br>         B2kFJJEBBP00,  .BGBNS00SBFvBBGB7   .rY   <br>        qBMNN8B:MkSBP    MqjYEPJZB. 7BBB.         <br>        :BBM08F  UE2:   :BZFLq2Pv    :L:          <br>          :BBY:  YBP,   EB8GB0OBu                 <br>           BBBJ BBBBr  BBqUGBBBNNM:               <br>          BMMME0B,vBL  Bq22MEYBBSEBu              <br>         MBBOFGBMZJB1  SPSXB,  PBBNZ              <br>         BB:iB2 iJ::BO:.0FPB.   qBZBL             <br>BMMMGP2YMB.  BBLi  7BBBBBMBB    BBBB              <br>j200MMMEGMBU jBBJ  FBBEMBBM    :B0Bq              <br>     ,rUUSNv  Y.   :X          7BGBk              <br>         .:7Y.    7i:,::,,..    MBP               <br>            .:.  i0kUUUUUFSqS2;iBB:               <br>                   ,iirir,,.,irLNMq               <br>" 
+fcontent[2]="                                                  <br>                                                  <br>             ,:                YFkU.              <br>            MMZE   ::         vBEGBB              <br>           :BY:v  vqY7       .GMFi1v              <br>          FBBBEE:iBBYL     vMBBBGNZL::            <br>         MBBBBB80BBBBBU.  YBMBBMBBBBG.            <br>        .BX0SZ8Bq0SGjOBB,:B0qM00PGXGBBON0F        <br>        .BMNGGu1MMM5kSBB.XBE8EEZZB5YBBBBB7SkFY,   <br>         BuS5LJEBBPG0:   B8BPF0qSB57BBZB.   :u:   <br>        iBEENXNFBP10U    M0YL0SYGB. YBBB          <br>        BBPN0BM :qqSF   .BGSjN5Nr    :r.          <br>        :BBq5OJ   BrM   MBGZB8OBi                 <br>          ,BJu. 7BBBE  .BZSkZB8GB                 <br>          5BMFu,OBBF    UBZUqMNYNO                <br>        :BBBBBBBM8Bi     :uqUuESUBS               <br>        UB,.M0U.YFBZ.     LM0NXE8kG               <br>EZG055j;JB. UBv   :L.   BBBBE1qMMPX               <br>SkMOBBMZkUSr.BM       0BBBBP7XBOBM.               <br>    .iLF15P5 vBB7     BBv    ZOBB                 <br>         .i7rUBY  77L7BBY . iBM.                  <br>             :,  :NqULYkJYJJMBG:                  <br>                   .::::ii;::LqBE7,               <br>" 
+fcontent[3]="                                                  <br>                                                  <br>                              .BBBBi              <br>           .0MN:   ri         FBSFMB              <br>           YBrLj  q5ji      .LBBS;F:              <br>          LBBUjr UBOU7     0BBBBBBBZu,            <br>         OBBBBBGMBBBBBj:  EBMBBMBMBBZ,            <br>        7BqMNBBZ5qPMYBBB.rBE0MqEqOuNBBBBBS.       <br>        YB8EO85BMMqYE5BM 8BBMZO8OB51BBBBY:1NZq.   <br>        iBSqk1Y8BBEN;    OMMFS0SFB1.BBBB     i    <br>        LMNNSukMPkZ8N,   MMLL8XYMX  :BBG          <br>        BBNOEM0rSuFZXB. :BMZPO0Bi                 <br>        XBBF5kO  ZY7BB7 kBBG8ZOB.                 <br>         .7BFk8  BJMBG   YBBZEqB:                 <br>          FBUiMYXBBB,      .BBBMi                 <br>         :BBBBMBBBMBv       UBZk7                 <br>          BBBB,:G8Y;;       MBBBv                 <br>NXXkUJv7,.iPBi             PBBBB                  <br>0ZBBBBBM8F7NB             5B0MB:                  <br>    :vUqXX1BB7            BBBk                    <br>         .:5BBL  .UujYJL7OBGB.                    <br>             .   iNEF1UUYkMBNkv7:                 <br>                   .::ii;::kEO:7UY,               <br>" 
+closetag='</font>'
+var ie4=document.all&&!document.getElementById
+var ns4=document.layers
+var DOM2=document.getElementById
+var index=0
+function changecontent(){
+if (index>=fcontent.length)
+index=0
+if (DOM2){
+document.getElementById("fscroller").innerHTML=begintag+fcontent[index].replace(/ /g,"&nbsp;")+closetag
+}
+else if (ie4)
+document.all.fscroller.innerHTML=begintag+fcontent[index].replace(/ /g,"&nbsp;")+closetag
+else if (ns4){
+document.fscrollerns.document.fscrollerns_sub.document.write(begintag+fcontent[index].replace(/ /g,"&nbsp;")+closetag)
+document.fscrollerns.document.fscrollerns_sub.document.close()
+}
+index++
+setTimeout("changecontent()",delay)
+}
+if (ie4||DOM2)
+document.write('<div id="fscroller"></div>')
+window.onload=changecontent
